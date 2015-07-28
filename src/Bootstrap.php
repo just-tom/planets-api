97b807dd5fc2ebc17afbf4901bcd33189b6cdbcb
Cranbri <tom@cranbri.co.uk>
2015-07-28 15:08:53 +0100

    formatted Boostrap to Symfony standartds, implemented rest of planet controller methods

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -5,12 +5,13 @@ namespace EMS;
 use Silex\Application;
 use EMS\Controllers\Providers as ControllerProvider;
 
-class Bootstrap extends Application{
+class Bootstrap extends Application
+{
     public function __construct()
     {
         parent::__construct();
         $app['debug'] = true;
-        
+
         $this->registerProviders();
         $this->registerControllers();
         $this->registerRoutes();
@@ -20,7 +21,9 @@ class Bootstrap extends Application{
     {
         $this->register(new \Silex\Provider\ServiceControllerServiceProvider());
         $this->register(new \JDesrosiers\Silex\Provider\CorsServiceProvider());
-        $this->register(new \Silex\Provider\DoctrineServiceProvider(), array(
+        $this->register(
+            new \Silex\Provider\DoctrineServiceProvider(),
+            array(
                 'db.options' => array(
                     'driver' => 'pdo_mysql',
                     'dbname' => 'ems_planets_local',
@@ -35,29 +38,39 @@ class Bootstrap extends Application{
     public function registerControllers()
     {
         $app = $this;
-        $this['planets.controller'] = $this->share(function() use ($app){
+        $this['planets.controller'] = $this->share(
+            function () use ($app) {
                 return new \EMS\Controllers\PlanetController($this);
-            });
-        $this['gases.controller'] = $this->share(function() use ($app){
+            }
+        );
+        $this['gases.controller'] = $this->share(
+            function () use ($app) {
                 return new \EMS\Controllers\GasController($this);
-            });
-        $this['satellites.controller'] = $this->share(function() use ($app){
+            }
+        );
+        $this['satellites.controller'] = $this->share(
+            function () use ($app) {
                 return new \EMS\Controllers\SatelliteController($this);
-            });
-        $this['planet_types.controller'] = $this->share(function() use ($app){
+            }
+        );
+        $this['planet_types.controller'] = $this->share(
+            function () use ($app) {
                 return new \EMS\Controllers\PlanetTypeController($this);
-            });
+            }
+        );
     }
 
     public function registerRoutes()
     {
         $app = $this;
-        $this->before(function() use ($app){
+        $this->before(
+            function () use ($app) {
                 $this['request_lang'] = 'json';
-                if($this['request']->get('lang') != null){
+                if ($this['request']->get('lang') != null) {
                     $this['request_lang'] = $this['request']->get('lang');
                 }
-            });
+            }
+        );
         $this->mount("/planets", new ControllerProvider\Planet());
         $this->mount("/gases", new ControllerProvider\Gas());
         $this->mount("/satellites", new ControllerProvider\Satellite());
--- src/Controllers/PlanetController.php
+++ src/Controllers/PlanetController.php
@@ -58,4 +58,72 @@ class PlanetController
 
         return $this->app->json($result);
     }
+
+    public function showSatellites($name)
+    {
+        $planetCollection = $this->app['db']->createQueryBuilder();
+        $planetCollection->select('*')->from('planets')->where('name = :name')
+            ->setParameter(':name', $name);
+        $handle = $planetCollection->execute();
+        $result['planet'] = $handle->fetch();
+
+        $satelliteCollection = $this->app['db']->createQueryBuilder();
+        $satelliteCollection->select('*')
+            ->from('satellites')
+            ->where('planet_id = :id')
+            ->setParameter(':id', $result['planet']['id']);
+        $handle = $satelliteCollection->execute();
+        $result['satellites'] = $handle->fetchAll();
+
+        return $this->app->json($result);
+    }
+
+    public function showSingleSatellite($name, $satellite)
+    {
+        $builder = $this->app['db']->createQueryBuilder();
+        $builder->select('s.*, p.*')
+            ->from('satellites', 's')
+            ->innerJoin('s', 'planets', 'p', 's.planet_id = p.id')
+            ->where('p.name = :name')
+            ->andWhere('s.name = :satellite')
+            ->setParameters(array(':name' => $name,':satellite' => $satellite));
+        $handle = $builder->execute();
+        $result = $handle->fetch();
+
+        return $this->app->json($result);
+    }
+
+    public function showPlanetTypes($name)
+    {
+        $planetCollection = $this->app['db']->createQueryBuilder();
+        $planetCollection->select('*')->from('planets')->where('name = :name')
+            ->setParameter(':name', $name);
+        $handle = $planetCollection->execute();
+        $result['planet'] = $handle->fetch();
+
+        $typeCollection = $this->app['db']->createQueryBuilder();
+        $typeCollection->select('*')
+            ->from('types')
+            ->where('id = :id')
+            ->setParameter(':id', $result['planet']['type_id']);
+        $handle = $typeCollection->execute();
+        $result['types'] = $handle->fetch();
+
+        return $this->app->json($result);
+    }
+
+    public function showSinglePlanetType($name, $type)
+    {
+        $builder = $this->app['db']->createQueryBuilder();
+        $builder->select('pt.*, p.*')
+            ->from('types', 'pt')
+            ->innerJoin('pt', 'planets', 'p', 'pt.id = p.type_id')
+            ->where('p.name = :name')
+            ->andWhere('pt.type = :type')
+            ->setParameters(array(':name' => $name,':type' => $type));
+        $handle = $builder->execute();
+        $result = $handle->fetch();
+
+        return $this->app->json($result);
+    }
 }
